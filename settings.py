6053a1c1de6a2c17bb8e9b67cbba106dda24dc99
Nick Sweeting <git@nicksweeting.com>
2017-12-28 03:34:24 -0500

    minor cleanup

--- settings.py
+++ settings.py
@@ -6,22 +6,23 @@ API_URL = 'https://api.gemini.com'
 API_WS_URL = 'wss://api.gemini.com'
 STARTING_NONCE = 120
 
-SYMBOL = 'ethusd'
-POLL_DELAY = 30
-USD_ORDER_AMT = 1.00
-MAX_LOSS_RATIO = -0.006
-MAX_GAIN_RATIO = 0.01
-MAX_ACTIVE_ORDERS = 3
-OVERPAY_RATIO = 0.005
+SYMBOL = 'ethusd'       # currency pair to trade
+POLL_DELAY = 30         # runloop interval in seconds
+USD_ORDER_AMT = 1.00    # amount to use when making new orders
+MAX_GAIN_RATIO = 0.01   # maximum percentage gains before selling the order
+MAX_LOSS_RATIO = -0.006 # maximum percentage losses before selling the order
+MAX_ACTIVE_ORDERS = 3   # maximum number of active orders to track
+OVERPAY_RATIO = 0.005   # percentage to pay over current price in order to guarantee orders closing quickly
 
-USD_MAX_NET_GAINS = 100
-USD_MAX_NET_LOSS = -20
+USD_MAX_NET_GAINS = 100  # total maximum USD gains before quitting the program
+USD_MAX_NET_LOSS = -20   # total maximum USD losses before quitting the program
 
-DATA_DIR = f'./data/{SYMBOL}'
+DATA_DIR = f'./data/{SYMBOL}'  # where to store the state and logs
+
+from secrets import *
+
 
 try:
     os.makedirs(DATA_DIR)
 except Exception:
     pass
-
-from secrets import *
