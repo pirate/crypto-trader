6672cd437673e994e963b7d5606a2acd551766b2
Nick Sweeting <git@nicksweeting.com>
2018-01-03 16:07:39 -0500

    lower overpay ratio slightly

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ MAX_ACTIVE_ORDERS = 5               # maximum number of active orders to track
 USD_MIN_ORDER_AMT = Decimal(5.00)   # min amount to use when making new orders
 USD_MAX_ORDER_AMT = Decimal(25.00)  # max amount to use when making new orders
 MAX_GAIN_RATIO = Decimal(0.02)      # maximum percentage gains before selling the order
-MAX_LOSS_RATIO = Decimal(-0.0075)    # maximum percentage losses before selling the order
-OVERPAY_RATIO = Decimal(0.005)      # percentage to pay over current price in order to guarantee orders closing quickly
+MAX_LOSS_RATIO = Decimal(-0.0075)   # maximum percentage losses before selling the order
+OVERPAY_RATIO = Decimal(0.0025)     # percentage to pay over current price in order to guarantee orders closing quickly
 
 USD_MAX_NET_GAINS = 10000           # total maximum USD gains before quitting the program
 USD_MAX_NET_LOSS = -20              # total maximum USD losses before quitting the program
