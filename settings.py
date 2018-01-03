c52d665c9284b8ae45dd9c51ad0febd88074ffe0
Nick Sweeting <git@nicksweeting.com>
2018-01-02 23:44:52 -0500

    increase trading amounts slightly

--- settings.py
+++ settings.py
@@ -8,11 +8,11 @@ STARTING_NONCE = 800                # must always increase incrementally
 
 SYMBOL = 'ethusd'                   # currency pair to trade
 POLL_DELAY = 30                     # runloop interval in seconds
-MAX_ACTIVE_ORDERS = 3               # maximum number of active orders to track
-USD_MIN_ORDER_AMT = Decimal(1.00)   # min amount to use when making new orders
-USD_MAX_ORDER_AMT = Decimal(5.00)   # max amount to use when making new orders
-MAX_GAIN_RATIO = Decimal(0.01)      # maximum percentage gains before selling the order
-MAX_LOSS_RATIO = Decimal(-0.006)    # maximum percentage losses before selling the order
+MAX_ACTIVE_ORDERS = 5               # maximum number of active orders to track
+USD_MIN_ORDER_AMT = Decimal(5.00)   # min amount to use when making new orders
+USD_MAX_ORDER_AMT = Decimal(25.00)  # max amount to use when making new orders
+MAX_GAIN_RATIO = Decimal(0.15)      # maximum percentage gains before selling the order
+MAX_LOSS_RATIO = Decimal(-0.05)     # maximum percentage losses before selling the order
 OVERPAY_RATIO = Decimal(0.005)      # percentage to pay over current price in order to guarantee orders closing quickly
 
 USD_MAX_NET_GAINS = 100             # total maximum USD gains before quitting the program
